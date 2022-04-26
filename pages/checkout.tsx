17923e8185d6f90608054aaf553e3d560cb8ab1a
Nolan Spencer <nspencer3513@gmail.com>
2022-04-25 20:31:26 -0400

    Checkout and contact page

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,7 +1,165 @@
-import { Box, Typography } from "@mui/material";
+import { Badge, Box, Button, ButtonGroup, Card, CardContent, CardMedia, Grid, IconButton, Skeleton, Typography } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import DeleteIcon from '@mui/icons-material/Delete';
+import React from "react";
 
 export default function Checkout() {
-    return <Box sx={{ p: 2 }}>
-        <Typography variant="h4">Checkout</Typography>
+    const [count, setCount] = React.useState(1);
+    return <Box sx={{ mx: 'auto', width: 'min(90vw)', textAlign: 'center', p: 2 }}>
+        <Typography variant="h2" style={{ textAlign: 'center', fontWeight: 'bold', marginTop: 2 }}>Checkout</Typography>
+        <hr/>
+        <Grid container spacing={2} sx={{ margin: 'auto', justifyContent: 'center' }}>
+            {/* Order Items */}
+            <Grid item xs={12} sm={6}>
+                <Box sx={{ border: 2, borderRadius: 2, height: '80vh', p: 1 }}>
+                    {/* A PRODUCT IN THE CART */}
+                    <Box sx={{ border: 2, borderRadius: 2 }}>
+                        <Card sx={{ display: 'flex' }}>
+                            <Skeleton variant="rectangular" width={151} height={140} />
+                            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                                <CardContent sx={{ flex: 'auto' }}>
+                                    <Typography component="div" variant="h5">
+                                        PRODUCT NAME
+                                    </Typography>
+                                    <Typography sx={{ alignText: 'left' }}>PRODUCT DESCRIPTION</Typography>
+                                    <Box sx={{ textAlign: 'left', mt: 1.5}}>
+                                        <ButtonGroup size="small" sx={{ textAlign: 'left'}}>
+                                        <Button
+                                                size="small"
+                                                aria-label="reduce"
+                                                onClick={() => {
+                                                    setCount(Math.max(count - 1, 0));
+                                                }}
+                                            >
+                                                <RemoveIcon fontSize="small" />
+                                            </Button>
+                                            <Button size="small" disableTouchRipple disableElevation>{count}</Button>
+                                            <Button
+                                                aria-label="increase"
+                                                onClick={() => {
+                                                    setCount(count + 1);
+                                                }}
+                                            >
+                                                <AddIcon fontSize="small" />
+                                            </Button>
+                                        </ButtonGroup>
+                                    </Box>
+                                </CardContent>
+                            </Box>
+                            <Box sx={{ display: 'flex', pl: 50, pb: 1, pt:1 }}>
+                                <DeleteIcon sx={{alignItems: 'right'}}/>
+                            </Box>
+                        </Card>
+                    </Box>
+                    <br />
+                    {/* PRODUCT PRODUCT PRODUCT */}
+                    {/* PRODUCT */}
+                    {/* PRODUCT PRODUCT PRODUCT */}
+                    {/* PRODUCT */}
+                    <Box sx={{ border: 2, borderRadius: 2 }}>
+                        <Card sx={{ display: 'flex' }}>
+                            <Skeleton variant="rectangular" width={151} height={140} />
+                            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                                <CardContent sx={{ flex: 'auto' }}>
+                                    <Typography component="div" variant="h5">
+                                        PRODUCT NAME
+                                    </Typography>
+                                    <Typography sx={{ alignText: 'left' }}>PRODUCT DESCRIPTION</Typography>
+                                    <Box sx={{ textAlign: 'left', mt: 1.5}}>
+                                        <ButtonGroup size="small" sx={{ textAlign: 'left'}}>
+                                        <Button
+                                                size="small"
+                                                aria-label="reduce"
+                                                onClick={() => {
+                                                    setCount(Math.max(count - 1, 0));
+                                                }}
+                                            >
+                                                <RemoveIcon fontSize="small" />
+                                            </Button>
+                                            <Button size="small" disableTouchRipple disableElevation>{count}</Button>
+                                            <Button
+                                                aria-label="increase"
+                                                onClick={() => {
+                                                    setCount(count + 1);
+                                                }}
+                                            >
+                                                <AddIcon fontSize="small" />
+                                            </Button>
+                                        </ButtonGroup>
+                                    </Box>
+                                </CardContent>
+                            </Box>
+                            <Box sx={{ display: 'flex', pl: 50, pb: 1, pt:1 }}>
+                                <DeleteIcon sx={{alignItems: 'right'}}/>
+                                <Typography variant="body1">PRICE:</Typography>
+                            </Box>
+                        </Card>
+                    </Box>
+                    {/* PRODUCT */}
+                </Box>
+            </Grid>
+            {/* Order Summary BOX */}
+            <Grid item xs={18} sm={4}>
+                <Box sx={{ border: 2, borderRadius: 2, height: '70vh' }}>
+                    <Typography variant="h3" sx={{ textAlign: 'left', p: 2 }}>Order Summary:</Typography>
+                    <Box sx={{ borderBottom: '4px solid' }}></Box>
+                    <Grid container spacing={1}>
+                        {/* ITEMS */}
+                        <Grid item xs={6} sm={8}>
+                            <Typography variant="h4" sx={{ textAlign: 'left', p: 1 }}>Items:</Typography>
+                        </Grid>
+                        <Grid item xs={6} sm={4}>
+                            <Typography variant="h4" sx={{ textAlign: 'right', p: 1 }}>$0000.00</Typography>
+                        </Grid>
+                        {/* Shipping and handling */}
+                        <Grid item xs={6} sm={8}>
+                            <Typography variant="h4" sx={{ textAlign: 'left', p: 1 }}>Shipping & Handling:</Typography>
+                        </Grid>
+                        <Grid item xs={6} sm={4}>
+                            <Typography variant="h4" sx={{ textAlign: 'right', p: 1 }}>$0000.00</Typography>
+                        </Grid>
+                        {/* Discount */}
+                        <Grid item xs={6} sm={8}>
+                            <Typography variant="h4" sx={{ textAlign: 'left', p: 1 }}>Discount:</Typography>
+                        </Grid>
+                        <Grid item xs={6} sm={4}>
+                            <Typography variant="h4" sx={{ textAlign: 'right', p: 1 }}>$0000.00</Typography>
+                        </Grid>
+                        <br /><br /><br /><br /><br /><br />
+                        {/* Total Before Tax */}
+                        <Grid item xs={6} sm={8}>
+                            <Typography variant="h4" sx={{ textAlign: 'left', p: 1 }}>Total before Tax:</Typography>
+                        </Grid>
+                        <Grid item xs={6} sm={4}>
+                            <Typography variant="h4" sx={{ textAlign: 'right', p: 1 }}>$0000.00</Typography>
+                        </Grid>
+                        {/* Estimated Tax */}
+                        <Grid item xs={6} sm={8}>
+                            <Typography variant="h4" sx={{ textAlign: 'left', p: 1 }}>Estimated Tax:</Typography>
+                        </Grid>
+                        <Grid item xs={6} sm={4}>
+                            <Typography variant="h4" sx={{ textAlign: 'right', p: 1 }}>$0000.00</Typography>
+                        </Grid>
+                        {/* LINE */}
+                        <Grid item xs={6} sm={12}>
+                            <Box sx={{ borderBottom: '4px solid' }}></Box>
+                        </Grid>
+                        {/* ORDER TOTAL */}
+                        <Grid item xs={6} sm={6}>
+                            <Typography variant="h3" sx={{ textAlign: 'left', p: 1, pt: 2 }}>Order Total:</Typography>
+                        </Grid>
+                        <Grid item xs={6} sm={6}>
+                            <Typography variant="h3" sx={{ textAlign: 'right', p: 1, pt: 2 }}>$0000.00</Typography>
+                        </Grid>
+                    </Grid>
+                </Box>
+                <Box sx={{ height: '10vh', p: 1 }}>
+                    <Button variant="contained" color="success" sx={{ height: '8vh', width: '500px', margin: 'auto', }}>
+                        <Typography variant="h2">Checkout</Typography>
+                    </Button>
+                </Box>
+            </Grid>
+        </Grid>
     </Box>
 }
