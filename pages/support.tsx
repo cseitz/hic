eb823a75dea63775fcaee198f1a8aea2a90a3cc1
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-04-25 21:22:37 -0400

    adjusted support

--- pages/support.tsx
+++ pages/support.tsx
@@ -1,42 +1,51 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Divider, Typography } from "@mui/material";
+import Link from "next/link";
+import { MaxWidth } from "./_app";
 
 export default function Support() {
-    return <Box sx={{ p: 2}}>
-        <Typography variant="h4" sx={{textAlign: "center"}} >Support</Typography>
-        <br/>
-        <Box sx = {{ p: 1}}>
-        <Typography variant="h5">Question 1</Typography>
-            <Typography variant="body1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis a cras semper auctor neque vitae tempus. Imperdiet proin fermentum leo vel orci porta. Tellus orci ac auctor augue mauris. Vestibulum lorem sed risus ultricies tristique nulla aliquet. Integer quis auctor elit sed vulputate mi sit amet mauris. Magna fringilla urna porttitor rhoncus dolor purus non. Consectetur lorem donec massa sapien faucibus et. Non pulvinar neque laoreet suspendisse interdum consectetur libero id. Purus sit amet volutpat consequat mauris nunc congue. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Commodo sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec. Odio morbi quis commodo odio aenean sed adipiscing diam. Viverra vitae congue eu consequat ac felis. Tellus molestie nunc non blandit massa enim nec dui. Tincidunt dui ut ornare lectus sit.
-
-Odio eu feugiat pretium nibh ipsum consequat nisl vel pretium. Sit amet risus nullam eget felis eget nunc lobortis mattis. Lectus proin nibh nisl condimentum id venenatis a. Porta lorem mollis aliquam ut porttitor leo a diam. Cras ornare arcu dui vivamus. Amet tellus cras adipiscing enim. Congue quisque egestas diam in arcu cursus euismod quis. Morbi non arcu risus quis varius. Eget mauris pharetra et ultrices neque ornare aenean. Enim ut sem viverra aliquet eget sit amet tellus cras. Velit egestas dui id ornare arcu odio ut. At elementum eu facilisis sed odio morbi quis commodo odio. Bibendum arcu vitae elementum curabitur vitae. Eget felis eget nunc lobortis mattis aliquam.</Typography>
+    return <Box sx={{ p: 2, mb: 10 }}>
+        <Box sx={{ ...MaxWidth, py: 5 }}>
+            <Typography variant="h4" sx={{ textAlign: "center" }} >Support</Typography>
+            <br />
+            <Box sx={{ p: 1 }}>
+                <Typography variant="h5">Question 1</Typography>
+                <Typography variant="body1">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis a cras semper auctor neque vitae tempus. Imperdiet proin fermentum leo vel orci porta. Tellus orci ac auctor augue mauris. Vestibulum lorem sed risus ultricies tristique nulla aliquet. Integer quis auctor elit sed vulputate mi sit amet mauris. Magna fringilla urna porttitor rhoncus dolor purus non. Consectetur lorem donec massa sapien faucibus et. Non pulvinar neque laoreet suspendisse interdum consectetur libero id. Purus sit amet volutpat consequat mauris nunc congue. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Commodo sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec. Odio morbi quis commodo odio aenean sed adipiscing diam. Viverra vitae congue eu consequat ac felis. Tellus molestie nunc non blandit massa enim nec dui. Tincidunt dui ut ornare lectus sit.
+                    Odio eu feugiat pretium nibh ipsum consequat nisl vel pretium. Sit amet risus nullam eget felis eget nunc lobortis mattis. Lectus proin nibh nisl condimentum id venenatis a. Porta lorem mollis aliquam ut porttitor leo a diam. Cras ornare arcu dui vivamus. Amet tellus cras adipiscing enim. Congue quisque egestas diam in arcu cursus euismod quis. Morbi non arcu risus quis varius. Eget mauris pharetra et ultrices neque ornare aenean. Enim ut sem viverra aliquet eget sit amet tellus cras. Velit egestas dui id ornare arcu odio ut. At elementum eu facilisis sed odio morbi quis commodo odio. Bibendum arcu vitae elementum curabitur vitae. Eget felis eget nunc lobortis mattis aliquam.
+                </Typography>
+            </Box>
+            <br />
+            <Box sx={{ p: 1 }}>
+                <Typography variant="h5">Question 2</Typography>
+                <Typography variant="body1">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis a cras semper auctor neque vitae tempus. Imperdiet proin fermentum leo vel orci porta. Tellus orci ac auctor augue mauris. Vestibulum lorem sed risus ultricies tristique nulla aliquet. Integer quis auctor elit sed vulputate mi sit amet mauris. Magna fringilla urna porttitor rhoncus dolor purus non. Consectetur lorem donec massa sapien faucibus et. Non pulvinar neque laoreet suspendisse interdum consectetur libero id. Purus sit amet volutpat consequat mauris nunc congue. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Commodo sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec. Odio morbi quis commodo odio aenean sed adipiscing diam. Viverra vitae congue eu consequat ac felis. Tellus molestie nunc non blandit massa enim nec dui. Tincidunt dui ut ornare lectus sit.
+                    Odio eu feugiat pretium nibh ipsum consequat nisl vel pretium. Sit amet risus nullam eget felis eget nunc lobortis mattis. Lectus proin nibh nisl condimentum id venenatis a. Porta lorem mollis aliquam ut porttitor leo a diam. Cras ornare arcu dui vivamus. Amet tellus cras adipiscing enim. Congue quisque egestas diam in arcu cursus euismod quis. Morbi non arcu risus quis varius. Eget mauris pharetra et ultrices neque ornare aenean. Enim ut sem viverra aliquet eget sit amet tellus cras. Velit egestas dui id ornare arcu odio ut. At elementum eu facilisis sed odio morbi quis commodo odio. Bibendum arcu vitae elementum curabitur vitae. Eget felis eget nunc lobortis mattis aliquam.
+                </Typography>
+            </Box>
+            <br />
+            <Box sx={{ p: 1 }}>
+                <Typography variant="h5">Question 3</Typography>
+                <Typography variant="body1">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis a cras semper auctor neque vitae tempus. Imperdiet proin fermentum leo vel orci porta. Tellus orci ac auctor augue mauris. Vestibulum lorem sed risus ultricies tristique nulla aliquet. Integer quis auctor elit sed vulputate mi sit amet mauris. Magna fringilla urna porttitor rhoncus dolor purus non. Consectetur lorem donec massa sapien faucibus et. Non pulvinar neque laoreet suspendisse interdum consectetur libero id. Purus sit amet volutpat consequat mauris nunc congue. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Commodo sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec. Odio morbi quis commodo odio aenean sed adipiscing diam. Viverra vitae congue eu consequat ac felis. Tellus molestie nunc non blandit massa enim nec dui. Tincidunt dui ut ornare lectus sit.
+                    Odio eu feugiat pretium nibh ipsum consequat nisl vel pretium. Sit amet risus nullam eget felis eget nunc lobortis mattis. Lectus proin nibh nisl condimentum id venenatis a. Porta lorem mollis aliquam ut porttitor leo a diam. Cras ornare arcu dui vivamus. Amet tellus cras adipiscing enim. Congue quisque egestas diam in arcu cursus euismod quis. Morbi non arcu risus quis varius. Eget mauris pharetra et ultrices neque ornare aenean. Enim ut sem viverra aliquet eget sit amet tellus cras. Velit egestas dui id ornare arcu odio ut. At elementum eu facilisis sed odio morbi quis commodo odio. Bibendum arcu vitae elementum curabitur vitae. Eget felis eget nunc lobortis mattis aliquam.
+                </Typography>
+            </Box>
+            <br />
+            <Box sx={{ p: 1 }}>
+                <Typography variant="h5">Question 4</Typography>
+                <Typography variant="body1">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis a cras semper auctor neque vitae tempus. Imperdiet proin fermentum leo vel orci porta. Tellus orci ac auctor augue mauris. Vestibulum lorem sed risus ultricies tristique nulla aliquet. Integer quis auctor elit sed vulputate mi sit amet mauris. Magna fringilla urna porttitor rhoncus dolor purus non. Consectetur lorem donec massa sapien faucibus et. Non pulvinar neque laoreet suspendisse interdum consectetur libero id. Purus sit amet volutpat consequat mauris nunc congue. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Commodo sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec. Odio morbi quis commodo odio aenean sed adipiscing diam. Viverra vitae congue eu consequat ac felis. Tellus molestie nunc non blandit massa enim nec dui. Tincidunt dui ut ornare lectus sit.
+                    Odio eu feugiat pretium nibh ipsum consequat nisl vel pretium. Sit amet risus nullam eget felis eget nunc lobortis mattis. Lectus proin nibh nisl condimentum id venenatis a. Porta lorem mollis aliquam ut porttitor leo a diam. Cras ornare arcu dui vivamus. Amet tellus cras adipiscing enim. Congue quisque egestas diam in arcu cursus euismod quis. Morbi non arcu risus quis varius. Eget mauris pharetra et ultrices neque ornare aenean. Enim ut sem viverra aliquet eget sit amet tellus cras. Velit egestas dui id ornare arcu odio ut. At elementum eu facilisis sed odio morbi quis commodo odio. Bibendum arcu vitae elementum curabitur vitae. Eget felis eget nunc lobortis mattis aliquam.
+                </Typography>
+            </Box>
         </Box>
-        <br/>
-        <Box sx = {{ p: 1}}>
-        <Typography variant="h5">Question 2</Typography>
-            <Typography variant="body1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis a cras semper auctor neque vitae tempus. Imperdiet proin fermentum leo vel orci porta. Tellus orci ac auctor augue mauris. Vestibulum lorem sed risus ultricies tristique nulla aliquet. Integer quis auctor elit sed vulputate mi sit amet mauris. Magna fringilla urna porttitor rhoncus dolor purus non. Consectetur lorem donec massa sapien faucibus et. Non pulvinar neque laoreet suspendisse interdum consectetur libero id. Purus sit amet volutpat consequat mauris nunc congue. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Commodo sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec. Odio morbi quis commodo odio aenean sed adipiscing diam. Viverra vitae congue eu consequat ac felis. Tellus molestie nunc non blandit massa enim nec dui. Tincidunt dui ut ornare lectus sit.
 
-Odio eu feugiat pretium nibh ipsum consequat nisl vel pretium. Sit amet risus nullam eget felis eget nunc lobortis mattis. Lectus proin nibh nisl condimentum id venenatis a. Porta lorem mollis aliquam ut porttitor leo a diam. Cras ornare arcu dui vivamus. Amet tellus cras adipiscing enim. Congue quisque egestas diam in arcu cursus euismod quis. Morbi non arcu risus quis varius. Eget mauris pharetra et ultrices neque ornare aenean. Enim ut sem viverra aliquet eget sit amet tellus cras. Velit egestas dui id ornare arcu odio ut. At elementum eu facilisis sed odio morbi quis commodo odio. Bibendum arcu vitae elementum curabitur vitae. Eget felis eget nunc lobortis mattis aliquam.</Typography>
-        </Box>
-        <br/>
-        <Box sx = {{ p: 1}}>
-        <Typography variant="h5">Question 3</Typography>
-            <Typography variant="body1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis a cras semper auctor neque vitae tempus. Imperdiet proin fermentum leo vel orci porta. Tellus orci ac auctor augue mauris. Vestibulum lorem sed risus ultricies tristique nulla aliquet. Integer quis auctor elit sed vulputate mi sit amet mauris. Magna fringilla urna porttitor rhoncus dolor purus non. Consectetur lorem donec massa sapien faucibus et. Non pulvinar neque laoreet suspendisse interdum consectetur libero id. Purus sit amet volutpat consequat mauris nunc congue. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Commodo sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec. Odio morbi quis commodo odio aenean sed adipiscing diam. Viverra vitae congue eu consequat ac felis. Tellus molestie nunc non blandit massa enim nec dui. Tincidunt dui ut ornare lectus sit.
+        <br />
 
-Odio eu feugiat pretium nibh ipsum consequat nisl vel pretium. Sit amet risus nullam eget felis eget nunc lobortis mattis. Lectus proin nibh nisl condimentum id venenatis a. Porta lorem mollis aliquam ut porttitor leo a diam. Cras ornare arcu dui vivamus. Amet tellus cras adipiscing enim. Congue quisque egestas diam in arcu cursus euismod quis. Morbi non arcu risus quis varius. Eget mauris pharetra et ultrices neque ornare aenean. Enim ut sem viverra aliquet eget sit amet tellus cras. Velit egestas dui id ornare arcu odio ut. At elementum eu facilisis sed odio morbi quis commodo odio. Bibendum arcu vitae elementum curabitur vitae. Eget felis eget nunc lobortis mattis aliquam.</Typography>
-        </Box>
-        <br/>
-        <Box sx = {{ p: 1}}>
-        <Typography variant="h5">Question 4</Typography>
-            <Typography variant="body1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis a cras semper auctor neque vitae tempus. Imperdiet proin fermentum leo vel orci porta. Tellus orci ac auctor augue mauris. Vestibulum lorem sed risus ultricies tristique nulla aliquet. Integer quis auctor elit sed vulputate mi sit amet mauris. Magna fringilla urna porttitor rhoncus dolor purus non. Consectetur lorem donec massa sapien faucibus et. Non pulvinar neque laoreet suspendisse interdum consectetur libero id. Purus sit amet volutpat consequat mauris nunc congue. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Commodo sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec. Odio morbi quis commodo odio aenean sed adipiscing diam. Viverra vitae congue eu consequat ac felis. Tellus molestie nunc non blandit massa enim nec dui. Tincidunt dui ut ornare lectus sit.
-
-Odio eu feugiat pretium nibh ipsum consequat nisl vel pretium. Sit amet risus nullam eget felis eget nunc lobortis mattis. Lectus proin nibh nisl condimentum id venenatis a. Porta lorem mollis aliquam ut porttitor leo a diam. Cras ornare arcu dui vivamus. Amet tellus cras adipiscing enim. Congue quisque egestas diam in arcu cursus euismod quis. Morbi non arcu risus quis varius. Eget mauris pharetra et ultrices neque ornare aenean. Enim ut sem viverra aliquet eget sit amet tellus cras. Velit egestas dui id ornare arcu odio ut. At elementum eu facilisis sed odio morbi quis commodo odio. Bibendum arcu vitae elementum curabitur vitae. Eget felis eget nunc lobortis mattis aliquam.</Typography>
-        </Box>
-        <Box sx = {{ borderBottom: '4px solid'}}></Box>
-
-        <br/>
-
-        <Box sx = {{textAlign: "center"}}>
-        <Button variant="contained" href = "/contact">Contact US</Button>
+        <Box sx={{ textAlign: "center" }}>
+            <Link href="/contact">
+                <Button variant="contained" href="/contact">Contact Us</Button>
+            </Link>
         </Box>
     </Box>
 }
