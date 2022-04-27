c972a59e21fa85b8d01f2f0593ea8d50d6a9521d
Nolan Spencer <nspencer@gpdgroup.com>
2022-04-27 13:51:15 -0400

    almost done

--- pages/products/[category].tsx
+++ pages/products/[category].tsx
@@ -1,10 +1,61 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Grid, Paper, Rating, Skeleton, Typography } from "@mui/material";
+import AddBoxIcon from '@mui/icons-material/AddBox';
 import { useRouter } from "next/router";
 
 export default function ProductCategory() {
-    const router = useRouter();
-    const { category } = router.query;
-    return <Box sx={{ p: 2 }}>
-        <Typography variant="h4">Products: {category}</Typography>
+  const router = useRouter();
+  const { category } = router.query;
+  return (
+    <Box sx={{mx: 'auto', width: 'min(90vw)', textAlign: 'center', p: 2, mb: 10  }}>
+      <Typography variant="h4">Products: {category}</Typography>
+      <Grid container sx={{margin: 'auto', justifyContent: 'center'}} rowSpacing={6} columnSpacing={2}>
+        <Grid item xs={6}>
+            <Product/>
+        </Grid>
+        <Grid item xs={6}>
+            <Product/>
+        </Grid>
+        <Grid item xs={6}>
+            <Product/>
+        </Grid>
+        <Grid item xs={6}>
+            <Product/>
+        </Grid>
+      </Grid>
     </Box>
+  );
+}
+function Product() {
+    return <Paper sx={{ borderRadius: 2, mb: 2 }}>
+        <Grid container>
+            <Grid item xs={4} xl={3}>
+                <Skeleton variant="rectangular" width={160} height={160} />
+            </Grid>
+            <Grid item xs={8} xl={9} sx={{ textAlign: 'left', px: 2, pt: 2 }}>
+                <Grid container>
+                    <Grid item xs={11}>
+                        <Box sx={{ minHeight: '60px', mb: 2 }}>
+                            <Typography component="div" variant="h5">
+                                PRODUCT NAME
+                            </Typography>
+                            <Rating name="read-only" value={3.5} readOnly />
+                        </Box>
+
+                    </Grid>
+                    <Grid item xs={1}>
+                    </Grid>
+                    <Grid item xs={11}>
+                        <Typography variant="h6" sx={{ textAlign: 'left' }}>Price: $0000.00</Typography>
+                    </Grid>
+                    <Grid item xs={1}>
+                        <AddBoxIcon sx={{ float: 'right', cursor: 'pointer', fontSize: 'large' }} />
+                    </Grid>
+                </Grid>
+
+                <Box sx={{ textAlign: 'left', mt: 1.5 }}>
+
+                </Box>
+            </Grid>
+        </Grid>
+    </Paper>;
 }
--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,7 +1,7 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Divider, Typography } from "@mui/material";
 
 export default function Products() {
     return <Box sx={{ p: 2 }}>
-        <Typography variant="h4">Products</Typography>
+        <Typography variant="h4" sx={{textAlign: 'center'}}>Product Categories</Typography>
     </Box>
 }
